141e9eeadab2a14efa6affebad2359afd5838347
KLari1994 <kelcee.lari@gmail.com>
2026-04-16 12:46:59 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ObjectionLab from './ObjectionLab.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ObjectionLab />
+  </React.StrictMode>
+)
